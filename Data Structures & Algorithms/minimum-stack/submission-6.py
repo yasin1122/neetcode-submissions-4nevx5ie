d61e9985c6eb6f7af161df3fa786e6f4151e2a8d
Yasin Cakal <whoisyasin@gmail.com>
2026-04-15 21:50:40 -0700

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,30 @@
+class MinStack:
+
+    def __init__(self):
+        # we create the empty stack here
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        # push using append
+        self.stack.append(val)
+        # if min_stack is empty push the val into it
+        # if val < min_stack[-1] push to min_stack
+        if not self.min_stack:
+            self.min_stack.append(val)
+        elif val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        # if we are popping min value, 
+        # pop from the min_stack as well
+        # pop using built in pop
+        if self.min_stack[-1] == self.stack.pop():
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        # return top element
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
