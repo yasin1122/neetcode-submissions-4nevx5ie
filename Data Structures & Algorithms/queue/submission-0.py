8b438fe63e4aca25d1ab17949401e34b789d2692
Yasin Cakal <whoisyasin@gmail.com>
2026-04-20 15:01:29 -0700

    Add: queue - submission-0

---
+++ Data Structures & Algorithms/queue/submission-0.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Deque:
+    
+    def __init__(self):
+        self.queue = deque()
+
+    def isEmpty(self) -> bool:
+        return not self.queue
+
+    def append(self, value: int) -> None:
+        self.queue.append(value)
+
+    def appendleft(self, value: int) -> None:
+        self.queue.appendleft(value)
+
+    def pop(self) -> int:
+        return self.queue.pop() if self.queue else -1
+
+    def popleft(self) -> int:
+        return self.queue.popleft() if self.queue else -1
