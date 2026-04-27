a0b8f887ff1e6ecaab74bb9d48e5255974d572c7
Yasin Cakal <whoisyasin@gmail.com>
2026-04-26 20:10:16 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map = {}
+
+        for char in s:
+            if char not in s_map:
+                s_map[char] = 1
+            else:
+                s_map[char] += 1
+         
+        for char in t:
+            if char not in t_map:
+                t_map[char] = 1
+            else:
+                t_map[char] += 1
+
+        return s_map == t_map
