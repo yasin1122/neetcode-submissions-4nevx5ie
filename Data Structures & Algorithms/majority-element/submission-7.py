402c5458d94be575bf8d695bc03e940ce3749dc6
Yasin Cakal <whoisyasin@gmail.com>
2026-04-27 22:13:47 -0700

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # voting system
+        # count up or down for curr element, 
+         # if vote = 0, new curr
+         # majority vote survives, return it
+
+        count = curr = 0
+
+        for num in nums:
+            if count == 0:
+                curr = num
+            count += 1 if curr == num else -1
+
+        return curr
