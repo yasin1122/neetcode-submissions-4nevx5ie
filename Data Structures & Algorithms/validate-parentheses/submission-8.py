490a0d2caf8fd7fe33f3d1172679c1c607d3dbba
Yasin Cakal <whoisyasin@gmail.com>
2026-05-07 22:17:36 -0700

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_to_close = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        stack = deque()
+
+        for char in s:
+            if char not in open_to_close:
+                stack.append(char)
+            elif stack and stack[-1] == open_to_close[char]:
+                stack.pop()
+            else:
+                return False
+
+        return not stack
