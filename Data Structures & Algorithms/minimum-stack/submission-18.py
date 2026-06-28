d60958449d516c8ef365dcc11330a5f722db4cd0
Yasin Cakal <whoisyasin@gmail.com>
2026-06-27 19:12:40 -0700

    Add: minimum-stack - submission-18

---
+++ Data Structures & Algorithms/minimum-stack/submission-18.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+        self.min_stack = deque()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.getMin():
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if not self.stack:
+            raise IndexError("Stack is Empty")
+        if self.stack.pop() == self.getMin():
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        if not self.stack:
+            raise IndexError("Stack is Empty")
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
