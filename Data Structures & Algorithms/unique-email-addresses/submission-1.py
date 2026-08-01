18bc1ea2f5b7439469e40ba5f76a3e92ce10f896
Yasin Cakal <whoisyasin@gmail.com>
2026-08-01 14:17:37 -0700

    Add: unique-email-addresses - submission-1

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique_emails = set()
+
+        for email in emails:
+            local, domain = email.split("@")
+
+            local = local.replace(".", "")
+            local = local.split("+")[0]
+
+            unique_emails.add(local + "@" + domain)
+
+        return len(unique_emails)
