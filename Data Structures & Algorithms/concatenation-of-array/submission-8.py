832ac1ed7aeab0410855ef18f5c1914305183bf8
Yasin Cakal <whoisyasin@gmail.com>
2026-08-25 13:53:25 -0700

    Add: concatenation-of-array - submission-8

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-8.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # create ans array 
+        # ans = nums + nums, then return it
+
+        return nums + nums
