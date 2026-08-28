a2c936b999486b342028047cf8e41123eb4f72c7
Yasin Cakal <whoisyasin@gmail.com>
2026-08-27 19:54:49 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,21 @@
+from collections import defaultdict, Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # sort each word and store as dict key
+        # if sorted version matches key, append to list of vals
+        # return all vals in dict as list of lists
+
+        anagrams = defaultdict(list)
+
+        # for word in strs:
+        #     anagrams["".join(sorted(word))].append(word)
+
+        for word in strs:
+            count = [0] * 26
+            for char in word:
+                count[ord(char) - ord("a")] += 1
+            anagrams[tuple(count)].append(word)
+
+        return list(anagrams.values())
+        
