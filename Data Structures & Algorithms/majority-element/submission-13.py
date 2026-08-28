03361b88d5e92c0515a26050202a667246317ff9
Yasin Cakal <whoisyasin@gmail.com>
2026-08-27 21:12:05 -0700

    Add: majority-element - submission-13

---
+++ Data Structures & Algorithms/majority-element/submission-13.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # return Counter(nums).most_common(1)[0][0]
+
+        nums.sort()
+        
+        return nums[len(nums) // 2]
