89292a3eab0aca3b95426697d51d526c9153b005
Yasin Cakal <whoisyasin@gmail.com>
2026-08-30 13:00:37 -0700

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_str = ""
+
+        for string in strs:
+            encoded_str += str(len(string)) + "@" + string
+        print(encoded_str)
+        return encoded_str
+
+    def decode(self, s: str) -> List[str]:
+        # find @, what comes before it is length
+        # extract length as int
+        # slice word and add it to list to return
+        word_list = []
+        i, j, length = 0, 0, 0
+
+        while j < len(s):
+            if s[j] != '@':
+                j += 1
+            else:
+                length = int(s[i:j])
+                i = j + 1
+                j = i + length
+                word_list.append(s[i:j])
+                i = j
+
+        print(word_list)
+
+        return word_list
