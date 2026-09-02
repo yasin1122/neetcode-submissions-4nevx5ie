5f0b978f615052005a9f6134dbf5ffa792fe1638
Yasin Cakal <whoisyasin@gmail.com>
2026-09-01 22:08:03 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,25 @@
+import math
+class Solution:
+    def distance(self, p):
+        return p[0] ** 2 + p[1] ** 2
+
+    def quicksort(self, arr, s, e):
+        if s >= e:
+            return arr
+
+        left = s
+        # pivot = arr[e]
+
+        for i in range(s, e):
+            if self.distance(arr[i]) < self.distance(arr[e]):
+                arr[i], arr[left] = arr[left], arr[i]
+                left += 1
+        arr[left], arr[e] = arr[e], arr[left]
+
+        self.quicksort(arr, s, left - 1)
+        self.quicksort(arr, left + 1, e)
+
+        return arr
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return self.quicksort(points, 0, len(points) - 1)[:k]
