93399691429b2a9398b8f68768666590ae563699
Yasin Cakal <whoisyasin@gmail.com>
2026-09-07 16:15:54 -0700

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-6

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-6.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # put inorder indices in a dict { val : i }
+        # root of curr BST is always preorder[0]
+        # mid of inorder is the index of root ^
+        # we recursively call dfs on left/right of mid 
+        indices = { val: i for i, val in enumerate(inorder) }
+        self.pre_idx = 0
+
+        def dfs(l, r):
+            if l > r:
+                return None
+
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+            root = TreeNode(root_val)
+            mid = indices[root_val]
+            root.left = dfs(l, mid - 1)
+            root.right = dfs(mid + 1, r)
+
+            return root
+
+        return dfs(0, len(preorder) - 1)
+
+        indices = {val: i for i, val in enumerate(inorder)} 
+        self.pre_idx = 0
+
+        def dfs(l, r):
+            if l > r:
+                return None
+
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+            root = TreeNode(root_val)
+            mid = indices[root_val] # O(1)
+            root.left = dfs(l, mid - 1)
+            root.right = dfs(mid + 1, r)
+            return root
+
+        return dfs(0, len(inorder) - 1)
+
+        # O(n^2) too slow
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1 : mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
+        return root
